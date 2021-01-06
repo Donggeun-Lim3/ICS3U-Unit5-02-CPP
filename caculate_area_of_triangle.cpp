edd53dfbe3950a51e85380294ed730ab2f9b78be
Ubuntu <ubuntu@ip-172-31-91-206.ec2.internal>
2021-01-06 01:48:24 +0000

    calculate area of triangle

---
+++ caculate_area_of_triangle.cpp
@@ -0,0 +1,48 @@
+// Copyright (C) 2002 Donggeun Lim All rights reserved.
+//
+// Created by: Donggeun Lim
+// Created on: Jan 2021
+// This program calculate area of triangle
+
+
+#include <iostream>
+#include <string>
+
+
+void CaculateArea(int base, int height) {
+    // this function calculate area of triangle
+
+    float area;
+
+    // process
+    area = 0.5 * base * height;
+
+    // output
+    std::cout << "The area of triangle is " << area << " cm²" << std::endl;
+}
+
+
+main() {
+    // this function gets base and height
+
+    std::string base;
+    std::string height;
+    int integerAsBase;
+    int integerAsHeight;
+
+    // input
+    std::cout << "Enter the base of triangle: ";
+    std::cin >> base;
+    std::cout << "Enter the height of triangle: ";
+    std::cin >> height;
+    std::cout << std::endl;
+
+    // call function
+    try {
+        integerAsBase = std::stoi(base);
+        integerAsHeight = std::stoi(height);
+        CaculateArea(integerAsBase, integerAsHeight);
+    } catch (std::invalid_argument) {
+        std::cout << "It is not an integer";
+    }
+}
